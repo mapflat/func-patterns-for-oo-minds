c859540ad317a02b08a0c3257b4d6997417bce19
Lars Albertsson <lalle@mapflat.com>
2016-07-29 17:10:27 +0200

    Add dependency examples.

--- src/main/java/com/mapflat/presentations/funcpatterns/JavaSlide.java
+++ src/main/java/com/mapflat/presentations/funcpatterns/JavaSlide.java
@@ -1,4 +1,9 @@
 package com.mapflat.presentations.funcpatterns;
 
+import com.mapflat.presentations.funcpatterns.JavaDeps.*;
+
 public class JavaSlide {
+    int example(ExampleClassDep arg) {
+        return 1;
+    }
 }
--- src/test/java/com/mapflat/presentations/funcpatterns/JavaSlideTest.java
+++ src/test/java/com/mapflat/presentations/funcpatterns/JavaSlideTest.java
@@ -1,7 +1,13 @@
 package com.mapflat.presentations.funcpatterns;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class JavaSlideTest {
-
+    @Test
+    public void exampleTest() {
+        JavaSlide slide = new JavaSlide();
+        assertEquals(1, slide.example(new JavaDeps.ExampleClassDep()));
+    }
 }

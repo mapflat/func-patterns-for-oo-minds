b2c938889f6960cb14eb49c635bcf339b54b738c
Lars Albertsson <lalle@mapflat.com>
2016-08-20 23:48:30 +0200

    Error slide work.

--- src/main/scala/com/mapflat/presentations/funcpatterns/ScalaSlide.scala
+++ src/main/scala/com/mapflat/presentations/funcpatterns/ScalaSlide.scala
@@ -1,5 +1,18 @@
 package com.mapflat.presentations.funcpatterns
 
-import ScalaDeps._
+import com.mapflat.presentations.funcpatterns.ScalaDeps._
 
-class ScalaSlide {}
+import scala.util.Try
+import scalaz._
+
+class ScalaSlide {
+
+  val maybeOpt: Option[Int] = riskyOpt()
+
+  val maybeEither: Either[Throwable, Int] = riskyEither()
+
+  val maybeScalazEither: \/[Throwable, Int] = riskyZEither()
+
+  val maybeTry: Try[Int] = riskyTry()
+
+}

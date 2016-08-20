7fa86c75d9fbb87bf14fc40fcf11ab8431b98985
Lars Albertsson <lalle@mapflat.com>
2016-08-20 23:48:30 +0200

    Error slide work.

--- src/test/scala/com/mapflat/presentations/funcpatterns/ScalaSlideTest.scala
+++ src/test/scala/com/mapflat/presentations/funcpatterns/ScalaSlideTest.scala
@@ -2,17 +2,5 @@ package com.mapflat.presentations.funcpatterns
 
 import org.scalatest.FlatSpec
 
-import ScalaDeps._
-
-import scala.util.Try
-
 class ScalaSlideTest extends FlatSpec {
-
-  val maybeOpt: Option[Int] = riskyOpt()
-
-  val maybeEither: Either[Throwable, Int] = riskyEither()
-
-  val maybeScalazEither: ZEither[Throwable, Int] = riskyZEither()
-
-  val maybeTry: Try[Int] = riskyTry()
 }

bf7a02d076f99ef34227dd362029752ccbc89dc0
Lars Albertsson <lalle@spotify.com>
2016-08-27 11:23:09 +0200

    Scala either slide work.

--- src/main/scala/com/mapflat/presentations/funcpatterns/ScalaSlide.scala
+++ src/main/scala/com/mapflat/presentations/funcpatterns/ScalaSlide.scala
@@ -1,5 +1,44 @@
 package com.mapflat.presentations.funcpatterns
 
-import ScalaDeps._
+import org.joda.time.DateTime
 
-class ScalaSlide {}
+class Event
+class Friend
+class Profile
+class Log {
+  def determineLastActive(): Either[Throwable, DateTime] = ???
+}
+
+trait ServiceProxy {
+  def retrieveSocialNetwork(): Either[Throwable, Set[Friend]] = ???
+
+  def retrieveActivityLog(): Either[Throwable, Log] = ???
+
+  def retrieveUserProfile(): Either[Throwable, Profile] = ???
+}
+
+class ScalaSlide {
+  // Error handling with Option. Swallows errors - bad idea.
+
+  class User2(val services: ServiceProxy) extends StrictLogging {
+
+    def sendPush(event: Event) = ???
+
+    def sendPushNotifications(): Unit = {
+      val eventsOrError: Either[Throwable, Set[Event]] = for {
+        userProfile <- services.retrieveUserProfile().right
+        activityLog <- services.retrieveActivityLog().right
+        lastActive <- activityLog.determineLastActive().right
+        friends <- services.retrieveSocialNetwork().right
+        events <- socialEvents(userProfile, lastActive, friends)
+      } yield events
+      eventsOrError.fold(
+        error => logger.error("Failed to push: ". error),
+        events => events.foreach(sendPush)
+      )
+    }
+
+    def socialEvents(profile: Profile, lastActive: DateTime, friends: Set[Friend]): Set[Event] = ???
+  }
+
+}

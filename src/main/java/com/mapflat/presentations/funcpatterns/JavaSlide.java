3c84e39883b37aaa409841bf47e0e2d50e7e6652
Lars Albertsson <lalle@spotify.com>
2016-08-30 14:38:58 +0200

    Work on json path slide.

--- src/main/java/com/mapflat/presentations/funcpatterns/JavaSlide.java
+++ src/main/java/com/mapflat/presentations/funcpatterns/JavaSlide.java
@@ -1,11 +1,21 @@
 package com.mapflat.presentations.funcpatterns;
 
+import com.jayway.jsonpath.JsonPath;
 import com.mapflat.presentations.funcpatterns.JavaDeps.*;
 
 import java.util.List;
 
 class JavaSlide {
-    List<String> authors(String json) {
-        return JsonPath.read(json, "$.store.book[*].author");
-    }
+  List<String> authors(String json) {
+    return JsonPath.read(json, "$.store.book[*].author");
+  }
+
+  List<String> cheapBooks(String json) {
+    return JsonPath.read(json, "$.store.book[?(@.price < 10)]");
+  }
+
+  int numBooks(String json) {
+    return JsonPath.read(json, "$..book.length()");
+  }
+
 }

3c7208d5c7f20d0d4e05f7ffdb7149c0d4862006
Lars Albertsson <lalle@spotify.com>
2016-08-30 23:00:50 +0200

    Argonaut lenses.

--- src/main/scala/com/mapflat/presentations/funcpatterns/ScalaSlide.scala
+++ src/main/scala/com/mapflat/presentations/funcpatterns/ScalaSlide.scala
@@ -1,5 +1,21 @@
 package com.mapflat.presentations.funcpatterns
 
-import ScalaDeps._
+import argonaut.Argonaut._
+import argonaut._
 
-class ScalaSlide {}
+import scala.util.{Failure, Success, Try}
+import scalaz.{-\/, PLens, \/, \/-}
+
+class ScalaSlide {
+  val streetLens = jObjectPL >=> jsonObjectPL("employees") >=> jArrayPL >=> jsonArrayPL(0) >=> jObjectPL >=> jsonObjectPL("address") >=> jObjectPL >=> jsonObjectPL("street") >=> jStringPL
+
+  val emailLens = jObjectPL >=> jsonObjectPL("employees") >=> jArrayPL >=> jsonArrayPL(0) >=> jObjectPL >=> jsonObjectPL("email") >=> jStringPL
+  val noteLens = jObjectPL >=> jsonObjectPL("employees") >=> jArrayPL >=> jsonArrayPL(0) >=> jObjectPL >=> jsonObjectPL("note") >=> jStringPL
+
+  def extractString(doc: String)(lens: PLens[Json, JsonString]): Try[Option[String]] = {
+    Parse.parse(doc).fold(
+      e => Failure(new IllegalArgumentException("Failed to parse json: " + e)),
+      json => Success(lens.get(json).map(_.toString))
+    )
+  }
+}

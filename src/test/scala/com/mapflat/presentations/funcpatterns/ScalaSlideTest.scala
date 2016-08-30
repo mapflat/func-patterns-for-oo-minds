24e5ccfda4986b18528f46d7007a12bd61a6a649
Lars Albertsson <lalle@spotify.com>
2016-08-30 23:00:50 +0200

    Argonaut lenses.

--- src/test/scala/com/mapflat/presentations/funcpatterns/ScalaSlideTest.scala
+++ src/test/scala/com/mapflat/presentations/funcpatterns/ScalaSlideTest.scala
@@ -2,47 +2,51 @@ package com.mapflat.presentations.funcpatterns
 
 import org.scalatest.FlatSpec
 
-import ScalaDeps._
+import scala.util.Success
 
 class ScalaSlideTest extends FlatSpec {
   val input =
-    """
-      |[
-      |  { "name": "Storbolaget",
-      |    "employees": [
-      |    { "name": "Pelle",
-      |      "address":
-      |      { "street": "Storgatan 1",
-      |        "zip": "123 45",
-      |        "city": "Stockholm" 
-      |      },
-      |      "email": "pelle@storb.com",
-      |      "salary": 30000
+    Vector("""
+      |{ "name": "Storbolaget",
+      |  "employees": [
+      |  { "name": "Ebba",
+      |    "address":
+      |    { "street": "Stortorget 2",
+      |      "zip": "123 56",
+      |      "city": "Solna",
+      |      "country": "Sweden"
       |    },
-      |    { "name": "Ebba",
-      |      "address":
-      |      { "street": "Stortorget 2",
-      |        "zip": "123 56",
-      |        "city": "Solna",
-      |        "country": "Sweden"
-      |      },
-      |      "email": "ebba@storb.com",
-      |      "note": "Private email ebba@gmail.com",
-      |      "salary": 35000
-      |    }
+      |    "email": "ebba@storb.com",
+      |    "note": "Private email: ebba@gmail.com",
+      |    "salary": 35000
       |  },
-      |  { "name": "Småbolaget",
-      |    "employees": [
-      |    { "name": "Anna",
-      |      "address":
-      |      { "street": "Lilla Gränd 3",
-      |        "zip": "123 67",
-      |        "city": "Sollentuna"
-      |      },
-      |      "email": "anna@smab.com",
+      |  { "name": "Pelle",
+      |    "address":
+      |    { "street": "Storgatan 1",
+      |      "zip": "123 45",
+      |      "city": "Stockholm"
       |    },
-      |  }
-      |]
-    """.stripMargin
+      |    "email": "pelle@storb.com",
+      |    "salary": 30000
+      |  }]
+      |}""", """
+      |{ "name": "Småbolaget",
+      |  "employees": [
+      |  { "name": "Anna",
+      |    "address":
+      |    { "street": "Lilla Gränd 3",
+      |      "zip": "123 67",
+      |      "city": "Sollentuna"
+      |    },
+      |    "email": "anna@smab.com"
+      |  }]
+      |}
+    """).map(_.stripMargin)
 
+  it should "work" in {
+    val slide = new ScalaSlide
+    assert(slide.extractString(input(0))(slide.streetLens) === Success(Some("Stortorget 2")))
+    assert(slide.extractString(input(1))(slide.emailLens) === Success(Some("anna@smab.com")))
+    assert(slide.extractString(input(1))(slide.noteLens) === Success(None))
+  }
 }

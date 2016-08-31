985a0353d4c2e27d41e53241167b4957de1882b4
Lars Albertsson <lalle@spotify.com>
2016-08-31 12:53:39 +0200

    Slide work

--- src/main/scala/com/mapflat/presentations/funcpatterns/ScalaSlide.scala
+++ src/main/scala/com/mapflat/presentations/funcpatterns/ScalaSlide.scala
@@ -1,15 +1,20 @@
 package com.mapflat.presentations.funcpatterns
 
+import monocle.Lens
 import monocle.macros.{GenLens, Lenses}
 import play.api.libs.json.{JsPath, Json, Reads}
 
 import scala.util.{Failure, Success, Try}
 
-@Lenses("get_") case class Address(street: String, zip: String, city: String, country: String)
-@Lenses("get_") case class Employee(name: String, address: Address, email: String, salary: Int, note: String)
+@Lenses case class Address(street: String, zip: String, city: String, country: String)
+@Lenses case class Employee(name: String, address: Address, email: String, salary: Int, note: String)
 case class Company(name: String, employees: Seq[Employee])
 
 class ScalaSlide {
+  implicit val jsonAddressReads = Json.reads[Address]
+  implicit val jsonEmployeeReads = Json.reads[Employee]
+  implicit val jsonCompanyReads = Json.reads[Company]
+
   def readCompany(doc: String): Try[Company] = {
     Json.fromJson[Company](Json.parse(doc)).fold(
       e => Failure(new IllegalArgumentException(e.toString())),
@@ -17,8 +22,14 @@ class ScalaSlide {
     )
   }
 
-//  val streetLens = JsPath \ "employees" \ 0 \ "address" \ "street"
-  val streetLens = GenLens[Company](_.employees(0)) composeLens Employee.get_address composeLens Address.get_street
-  def emailLens(i: Int) = GenLens[Company](_.employees(i)) ^|-> Employee.get_email
+  val companyNameLens = GenLens[Company](_.name)
+
+  def employeeLens(i: Int) = Lens[Company, Employee](
+    _.employees(i))( // Getter
+    e => c => c.copy(employees = c.employees.take(i) ++ Seq(e) ++ c.employees.drop(i + 1)))  // Setter
+
+  def streetLens(i: Int) = employeeLens(i) composeLens Employee.address composeLens Address.street
+
+  def emailLens(i: Int) = employeeLens(i) ^|-> Employee.email
 
 }

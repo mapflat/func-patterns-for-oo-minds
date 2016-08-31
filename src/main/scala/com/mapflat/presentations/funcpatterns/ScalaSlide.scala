09bd2412f97bcbad62cddaeab2df4fd9fcb0cee1
Lars Albertsson <lalle@spotify.com>
2016-08-31 09:40:15 +0200

    Slide work

--- src/main/scala/com/mapflat/presentations/funcpatterns/ScalaSlide.scala
+++ src/main/scala/com/mapflat/presentations/funcpatterns/ScalaSlide.scala
@@ -6,16 +6,12 @@ import scala.util.{Success, Try}
 
 class ScalaSlide {
   val streetLens = JsPath \ "employees" \ 0 \ "address" \ "street"
-
-//  val emailLens = jObjectPL >=> jsonObjectPL("employees") >=> jArrayPL >=> jsonArrayPL(0) >=> jObjectPL >=> jsonObjectPL("email") >=> jStringPL
-//  val noteLens = jObjectPL >=> jsonObjectPL("employees") >=> jArrayPL >=> jsonArrayPL(0) >=> jObjectPL >=> jsonObjectPL("note") >=> jStringPL
+  val emailLens = JsPath \ "employees" \ 0 \ "email"
+  val noteLens = JsPath \ "employees" \ 0 \ "note"
 
   def extractString(doc: String)(lens: JsPath): Try[Option[String]] = {
-    Success(Json.parse(doc).validate(lens.read[String]).asOpt)
-
-//    Parse.parse(doc).fold(
-//      e => Failure(new IllegalArgumentException("Failed to parse json: " + e)),
-//      json => Success(lens.get(json).map(_.toString))
-
+    Try {
+      Json.parse(doc).validate(lens.read[String]).asOpt
+    }
   }
 }

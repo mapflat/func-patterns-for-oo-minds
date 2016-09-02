e29276f268e5cf2621ea69376775c0fc8d6eaec1
Lars Albertsson <lalle@spotify.com>
2016-09-02 22:28:22 +0200

    Merge func-rising.

---
+++ slides/func-rising/ScalaDeps.scala
@@ -0,0 +1,122 @@
+package com.mapflat.presentations.funcpatterns
+
+object ScalaDeps {
+  object Schibsted {
+    def Products(a: Any): Products_ = ???
+  }
+
+  object Technology {
+    def apply(a: Any): Int = 0
+  }
+
+  object Google {
+
+    def apply(a: Any): Unit = ???
+  }
+
+  object Hangouts
+
+  object Recorded {
+    def Future(v: Any) = ???
+  }
+
+  object Natural {
+    def Language(v: Any) = ???
+  }
+
+  object Processing
+
+  object Cinnober {
+    def apply(a: Any) = ???
+  }
+
+  object Stock {
+    def Exchange(a: Any) = ???
+  }
+
+  object Systems
+
+  object Spotify {
+    def apply(a: Any, a2: Any) = ???
+  }
+
+  object Data {
+    def Engineering = ???
+
+    def Modelling = ???
+
+    def Architect = ???
+  }
+
+
+  object Lars {
+    def Albertsson(value: Any) = ???
+
+  }
+
+  object Rouzbeh {
+    def Delawari(a: Any) = ???
+  }
+
+  object Swedish {
+
+    def Institute(Of: Any): Institute_ = ???
+  }
+
+
+  class Institute_ {
+    def Computer(value: Any) = ???
+  }
+
+  object Science {
+    def apply(value: Nothing): Any = ???
+  }
+
+  class Computer_ {
+  }
+
+  object Of
+
+  object Testing {
+    def +(Debugging: Debugging.type): TestDebug = ???
+  }
+
+  object Debugging
+
+  class TestDebug {
+    def of(value: Any) = ???
+  }
+
+  object Distributed {
+    def Systems = ???
+  }
+
+  object Sun {
+    def Microsystems(v: Any) = ???
+  }
+
+  object Server {
+    def Verification = ???
+  }
+
+  object & {
+  }
+
+  class Products_ {
+
+    def Technology(a: Any) = ???
+  }
+
+  object Independent {
+    def Consultant(a1: Any, a2: Any) = ???
+  }
+
+  object Architecture {
+    def &(a: Any) = ???
+  }
+
+  object Strategy
+
+
+
+}

e5409c7cf979d23b128f3af2854a4d86649b3738
Lars Albertsson <lalle@spotify.com>
2016-09-03 00:36:44 +0200

    Add monocle slide.

---
+++ slides/monocle/ScalaSlideTest.scala
@@ -0,0 +1,54 @@
+package com.mapflat.presentations.funcpatterns
+
+import org.scalatest.FlatSpec
+import scala.util.Success
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ScalaSlideTest extends FlatSpec {
+  val input =
+    Vector("""
+      |{ "name": "Storbolaget",
+      |  "employees": [
+      |  { "name": "Ebba",
+      |    "address":
+      |    { "street": "Stortorget 2",
+      |      "zip": "123 56",
+      |      "city": "Solna",
+      |      "country": "Sweden"
+      |    },
+      |    "email": "ebba@storb.com",
+      |    "note": "Private email: ebba@gmail.com",
+      |    "salary": 35000
+      |  },
+      |  { "name": "Pelle",
+      |    "address":
+      |    { "street": "Storgatan 1",
+      |      "zip": "123 45",
+      |      "city": "Stockholm"
+      |    },
+      |    "email": "pelle@storb.com",
+      |    "salary": 30000
+      |  }]
+      |}""", """
+      |{ "name": "Småbolaget",
+      |  "employees": [
+      |  { "name": "Anna",
+      |    "address":
+      |    { "street": "Lilla Gränd 3",
+      |      "zip": "123 67",
+      |      "city": "Sollentuna"
+      |    },
+      |    "email": "anna@smab.com"
+      |  }]
+      |}
+    """).map(_.stripMargin)
+
+  it should "work" in {
+    val slide = new ScalaSlide
+    val tryC = slide.readCompany(input(0))
+    assert(tryC.map(c => slide.streetLens(0).get(c)) === Success("Stortorget 2"))
+    assert(tryC.map(c => slide.emailLens(1).get(c)) === Success("pelle@storb.com"))
+  }
+}

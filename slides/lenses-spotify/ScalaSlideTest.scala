0362dfaeefd3e0841f8f31386e917727096f3f1e
Lars Albertsson <lalle@spotify.com>
2016-09-03 00:40:23 +0200

    Add spotify lens slide.

---
+++ slides/lenses-spotify/ScalaSlideTest.scala
@@ -0,0 +1,9 @@
+package com.mapflat.presentations.funcpatterns
+
+import org.junit.runner.RunWith
+import org.scalatest.FlatSpec
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ScalaSlideTest extends FlatSpec {
+}

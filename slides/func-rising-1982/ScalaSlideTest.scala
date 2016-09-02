78e4dc874ac83977e73f9331e8704b1f6592db29
Lars Albertsson <lalle@spotify.com>
2016-09-02 22:42:21 +0200

    Merge func-rising 1982 .

---
+++ slides/func-rising-1982/ScalaSlideTest.scala
@@ -0,0 +1,9 @@
+package com.mapflat.presentations.funcpatterns
+
+import org.scalatest.FlatSpec
+
+import ScalaDeps._
+
+class ScalaSlideTest extends FlatSpec {
+  // And tests will be shown here.
+}

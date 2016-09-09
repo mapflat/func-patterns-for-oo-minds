97f00fa7f6d9e81bb25176302a8a326039e055f8
Lars Albertsson <lalle@spotify.com>
2016-09-09 09:36:41 +0200

    Add func patterns slide.

---
+++ slides/func-patterns/ScalaDeps.scala
@@ -0,0 +1,8 @@
+package com.mapflat.presentations.funcpatterns
+
+object ScalaDeps {
+  def prev_slide = ???
+  def next_slide = ???
+  def move_to_slide = ???
+
+}
---
+++ slides/func-patterns/ScalaSlide.scala
@@ -0,0 +1,32 @@
+package com.mapflat.presentations.funcpatterns
+
+import ScalaDeps._
+import org.apache.commons.lang3.text.WordUtils
+
+class ScalaSlide {
+  "Gang of Four patterns - some are trivial with functional constructs"
+
+
+  val commandPattern = Map("move" -> move_to_slide, "next" -> next_slide, "prev" -> prev_slide)
+
+
+  def adapterPattern(formatter: (Int, String) => String) =
+    (text: String, width: Int) => formatter(width, text)
+
+
+  def observerPattern(text: String, width: Int, lineStatsCollector: (Int) => Unit = {_}): String = {
+    val wrapped = WordUtils.wrap(text, width)
+    lineStatsCollector(wrapped.split("\n").size)  // Observe line count, ignored by default
+    wrapped
+  }
+
+
+  // Chain of responsibility pattern.
+  // Clearer?  Depends on taste.
+  def authenticate(user: String, authenticators: Seq[(String) => Boolean]) =
+    authenticators.map(auth => auth(user)).fold(false)((a, b) => a || b)
+
+  
+  "Likewise with Visitor, Decorator, Interpreter, Builder, Prototype, ..."
+}
+

c358f71dbf97fc62e01cdc7d34b094330c583fb2
Lars Albertsson <lalle@spotify.com>
2016-09-14 12:42:51 +0200

    Add video link.

--- slides/title/ScalaSlide.scala
+++ slides/title/ScalaSlide.scala
@@ -10,7 +10,9 @@ class ScalaSlide {
     Gems that I have discovered on brief excursions into functional programming,
     and dragons to avoid.
 
-    Code on <https://github.com/mapflat/func-patterns-for-oo-minds>
+    Code on https://github.com/mapflat/func-patterns-for-oo-minds
+
+    Video from JavaZone 2016 at https://2016.javazone.no/program/functional-patterns-for-object-oriented-minds
 """
 }
 

4c18fc2ec2c5fccaedb1c4ddfcd6a37e1a7eb629
Lars Albertsson <lalle@spotify.com>
2016-09-14 12:07:09 +0200

    Minor tweaks.

--- slides/title/ScalaSlide.scala
+++ slides/title/ScalaSlide.scala
@@ -5,9 +5,10 @@ class ScalaSlide {
   val title = "Functional patterns for object-oriented minds"
 
   val summary = """
-    Lars Albertsson (independent)
+    Lars Albertsson (independent, mapflat.com)
 
-    Gems that I have discovered on brief excursions into functional programming.
+    Gems that I have discovered on brief excursions into functional programming,
+    and dragons to avoid.
 
     Code on <https://github.com/mapflat/func-patterns-for-oo-minds>
 """
